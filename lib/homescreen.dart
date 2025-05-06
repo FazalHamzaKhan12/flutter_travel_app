435205719ff966aa638359dd795be63c19f09472
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-05-06 10:59:17 +0500

    working

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -5,6 +5,295 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            height: 130,
+            width: double.infinity,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0, top: 26),
+                      child: Text(
+                        "Hi, David 👋",
+                        style: TextStyle(
+                          color: Color(0xff2F2F2F),
+                          fontSize: 27,
+                          fontFamily: 'Montserrats',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 220.0, top: 30),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUpeorPvZN_kz2u_YMpnlcw5sdu2OfQKZ8NQ&s",
+                        ),
+                        radius: 26,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 295.0),
+                  child: Text(
+                    "Explore the world",
+                    style: TextStyle(
+                      color: Color(0xff888888),
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 14),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(color: Colors.grey.shade400),
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search places',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 30,
+                    width: 1,
+                    color: Colors.grey.shade400,
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                  ),
+                  Icon(Icons.tune, color: Colors.grey.shade600),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 22.0, top: 20),
+                child: Text(
+                  "Popular places",
+                  style: TextStyle(
+                    color: Color(0xff2F2F2F),
+                    fontSize: 20,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 220.0, top: 20),
+                child: Text(
+                  "View all",
+                  style: TextStyle(
+                    color: Color(0xff888888),
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 10),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  FilterButton(title: 'Most Viewed', isSelected: true),
+                  SizedBox(width: 15),
+                  FilterButton(title: 'Nearby', isSelected: false),
+                  SizedBox(width: 15),
+                  FilterButton(title: 'Latest', isSelected: false),
+                  SizedBox(width: 15),
+                  FilterButton(title: 'Trending', isSelected: false),
+                  SizedBox(width: 15),
+                  FilterButton(title: 'New Added', isSelected: false),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+
+          Stack(
+            children: [
+              // Background image card
+              Container(
+                height: 350,
+                width: 250,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://epicexpeditions.co/wp-content/uploads/2022/09/k2_base_camp_trek11.jpg",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(25),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      color: Colors.black.withOpacity(0.3),
+                      offset: Offset(2, 4),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Heart icon at top-right
+              Positioned(
+                top: 12,
+                right: 12,
+                child: Container(
+                  padding: EdgeInsets.all(6),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(
+                      176,
+                      255,
+                      255,
+                      255,
+                    ).withOpacity(0.9),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(
+                    Icons.favorite_border,
+                    size: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+              // Bottom info card
+              Positioned(
+                bottom: 12,
+                left: 12,
+                right: 12,
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Mount Fuji",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            TextSpan(
+                              text: ", Tokyo",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.white,
+                                size: 16,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Tokyo, Japan",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star_border,
+                                color: Colors.white,
+                                size: 16,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                "4.8",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FilterButton extends StatelessWidget {
+  final String title;
+  final bool isSelected;
+
+  const FilterButton({Key? key, required this.title, required this.isSelected})
+    : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        color: isSelected ? Colors.black : Colors.grey.shade100,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Text(
+        title,
+        style: TextStyle(
+          color: isSelected ? Colors.white : Colors.grey,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
   }
 }
